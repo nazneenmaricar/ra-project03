a6b0b524fe12cfa3186f43471415b26461bbd78d
Nazneen Samu Maricar <nazneenmaricar@Nazs-MacBook-Pro.local>
2017-05-01 10:42:47 -0400

    TEST

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 /**
  * Created by Edward_J_Apostol on 2017-04-28.
  */
-console.log("this is a test");
+require('file-loader?name=../dist/index.html!./index.html');
+
+console.log("this is a test... this isn't even---loading");
+
+console.log("wyf");
 
 import App from './App'
 
